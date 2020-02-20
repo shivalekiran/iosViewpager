2686a67644e6423fb99da4781e7e65e142a52e10
shivalekiran <kiransshivale@gmail.com>
2020-02-20 17:29:33 +0530

    iOS view pager demo

---
+++ ViewPagerDemo/Slide.swift
@@ -0,0 +1,15 @@
+//
+//  Slide.swift
+//  ViewPagerDemo
+//
+//  Created by Kiran on 20/02/20.
+//  Copyright © 2020 Fideltech. All rights reserved.
+//
+
+import UIKit
+
+class Slide: UIView {
+    @IBOutlet weak var slideTextView: UITextView!
+    @IBOutlet weak var slideImageView:UIImageView!
+    
+}
